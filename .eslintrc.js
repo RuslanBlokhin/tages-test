c37397547da075e2a93ffb411921a18bfa3d2f1a
Ruslan Blokhin <blokhin.rus@gmail.com>
2024-03-25 14:56:59 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  endOfLine: 'auto',
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  rules: {
+    'vue/attributes-order': [
+      'error',
+      {
+        order: ['DEFINITION', 'LIST_RENDERING', 'CONDITIONALS', 'RENDER_MODIFIERS', 'GLOBAL', ['UNIQUE', 'SLOT'], 'TWO_WAY_BINDING', 'OTHER_DIRECTIVES', 'OTHER_ATTR', 'EVENTS', 'CONTENT'],
+        alphabetical: false,
+      },
+    ],
+  },
+};
